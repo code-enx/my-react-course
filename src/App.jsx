d6ab2f5a5dfedba04f685db681e57d52c337cdcf
code-enx <codeenx@gmail.com>
2025-09-06 22:33:03 +0530

    Day 3: Creating components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-import Greet from "./components/Greet";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from ".components/Footer";
 
 function App() {
   return (
     <div>
-      <Greet />
+      <Header />
+      <Main />
+      <Footer />
     </div>
   );
 }
--- src/components/Greet.jsx
+++
@@ -1,5 +0,0 @@
-function Greet() {
-  return <h1>enix</h1>;
-}
-
-export default Greet;
