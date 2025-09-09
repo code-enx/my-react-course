ebc5cebe7972c54aa3d5a401e163fb30f3362d42
code-enx <codeenx@gmail.com>
2025-09-09 09:54:18 +0530

    Day 6: Learning list in react

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,45 @@
-import Greeting from "./components/Greeting";
-import ProductInfo from "./components/ProductInfo";
-
 function App() {
+  const usersInfo = [
+    {
+      username: "enix",
+      email: "codeenx@gmail.com",
+      location: "California",
+    },
+
+    {
+      username: "Xe54z",
+      email: "xe54@gmail.com",
+      location: "Los Angels",
+    },
+
+    {
+      username: "stefin",
+      email: "stefin@gmail.com",
+      location: "Denmark",
+    },
+
+    {
+      username: "Ben",
+      email: "bensharon@gmail.com",
+      location: "Santa Monica",
+    },
+
+    {
+      username: "Asra",
+      email: "asra@gmail.com",
+      location: "NewYork",
+    },
+  ];
   return (
-    <div>
-      <Greeting />
-      <ProductInfo />
-    </div>
+    <main>
+      {usersInfo.map((user) => (
+        <ul key={Math.random()}>
+          <li>{user.username}</li>
+          <li>{user.email}</li>
+          <li>{user.location}</li>
+        </ul>
+      ))}
+    </main>
   );
 }
 
