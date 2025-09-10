3b40429388b326dd280c566d43d74b5d958b1669
code-enx <codeenx@gmail.com>
2025-09-10 23:23:50 +0530

    Day 7: Learning react II

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,10 @@
+import UserList from "./components/UserList";
+
 function App() {
-  const usersInfo = [
-    {
-      username: "enix",
-      email: "codeenx@gmail.com",
-      location: "California",
-    },
-
-    {
-      username: "Xe54z",
-      email: "xe54@gmail.com",
-      location: "Los Angels",
-    },
-
-    {
-      username: "stefin",
-      email: "stefin@gmail.com",
-      location: "Denmark",
-    },
-
-    {
-      username: "Ben",
-      email: "bensharon@gmail.com",
-      location: "Santa Monica",
-    },
-
-    {
-      username: "Asra",
-      email: "asra@gmail.com",
-      location: "NewYork",
-    },
-  ];
   return (
-    <main>
-      {usersInfo.map((user) => (
-        <ul key={Math.random()}>
-          <li>{user.username}</li>
-          <li>{user.email}</li>
-          <li>{user.location}</li>
-        </ul>
-      ))}
-    </main>
+    <div>
+      <UserList />
+    </div>
   );
 }
 
