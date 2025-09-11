66772c7fcf19a2d519816634d4babcad4ee80247
code-enx <codeenx@gmail.com>
2025-09-11 21:36:34 +0530

    Day 8: Learning Props

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-import UserList from "./components/UserList";
+import Person from "./components/Person";
+import Products from "./components/Products";
 
 function App() {
   return (
     <div>
-      <UserList />
+      <div>
+        <Person name="enix" age={20} />
+        <Person name="xe54z" age={20} />
+      </div>
+      <div>
+        <Products name="Maingear" price="2300" />
+        <Products name="System 76" price="3000" />
+      </div>
     </div>
   );
 }
