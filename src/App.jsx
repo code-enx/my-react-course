e22de0042dcffaee6a7b70c61a871fd29dbb0fb8
code-enx <codeenx@gmail.com>
2025-09-12 23:52:25 +0530

    Day 9: Learning conditions

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
-import Person from "./components/Person";
-import Products from "./components/Products";
+import Whether from "./components/Whether";
+import UserStatus from "./components/UserStatus";
 
 function App() {
   return (
     <div>
-      <div>
-        <Person name="enix" age={20} />
-        <Person name="xe54z" age={20} />
-      </div>
-      <div>
-        <Products name="Maingear" price="2300" />
-        <Products name="System 76" price="3000" />
-      </div>
+      <Whether />
+      <UserStatus loggedIn={true} isAdmin={false} />
     </div>
   );
 }
---
+++ src/components/UserStatus.jsx
@@ -0,0 +1,9 @@
+function UserStatus(props) {
+  if (props.loggedIn && props.isAdmin) {
+    return <h1>Welcome Admin</h1>;
+  } else {
+    return <h1>Welcome user</h1>;
+  }
+}
+
+export default UserStatus;
